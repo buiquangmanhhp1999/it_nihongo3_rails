d1636a62157721ee801ffbc87a69bf1c6e622e94
Ubuntu <ubuntu@ip-172-31-84-81.ec2.internal>
2020-11-28 18:18:08 +0000

    Add detail book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_160037) do
+ActiveRecord::Schema.define(version: 2020_11_28_180519) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -18,9 +18,6 @@ ActiveRecord::Schema.define(version: 2020_11_24_160037) do
     t.integer "record_id", null: false
     t.integer "blob_id", null: false
     t.datetime "created_at", null: false
-    t.string "name"
-    t.integer "age"
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -41,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_160037) do
     t.integer "number̲of̲pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
